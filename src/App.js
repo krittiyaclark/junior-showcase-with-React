e94768c9679bee0ee57f304345274ef7fbf0ef35
krittiyaclark <krittiyaclark@gmail.com>
2019-04-21 07:35:23 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Junior Showcase</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
