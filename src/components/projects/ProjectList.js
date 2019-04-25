754585485ab0c5c7d291f3c78ec395028af38cbf
krittiyaclark <krittiyaclark@gmail.com>
2019-04-25 09:14:56 -0400

    Project summary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,30 +1,16 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card project-summary">
-                <div className="card-content text-secondary">
-                    <h5 className="card-title">Project Title</h5>
-                    <p className="card-text">Posted by the Wippo</p>
-                    <p className="text-secondary">1st July, 9am</p>
-                </div>
-            </div>
-            <div className="card project-summary">
-                <div className="card-content text-secondary">
-                    <h5 className="card-title">Project Title</h5>
-                    <p className="card-text">Posted by the Wippo</p>
-                    <p className="text-secondary">1st July, 9am</p>
-                </div>
-            </div>
-            <div className="card project-summary">
-                <div className="card-content text-secondary">
-                    <h5 className="card-title">Project Title</h5>
-                    <p className="card-text">Posted by the Wippo</p>
-                    <p className="text-secondary">1st July, 9am</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
+
     )
 }
 
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return(
+        <div className="card project-summary">
+            <div className="card-content text-secondary">
+                <h5 className="card-title">Project Title</h5>
+                <p className="card-text">Posted by the Wippo</p>
+                <p className="text-secondary">1st July, 9am</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
