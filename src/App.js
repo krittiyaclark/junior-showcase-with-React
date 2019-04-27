4861f151ac26e72ea4b58f30e7baf08fd44da364
krittiyaclark <krittiyaclark@gmail.com>
2019-04-27 15:45:21 -0400

    Create project form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 class App extends Component {
   render() {
@@ -17,7 +18,7 @@ class App extends Component {
             <Route path="/project/:id" component={ ProjectDetails } />
             <Route path="/signIn" component={ SignIn } />
             <Route path="/signUp" component={ SignUp } />
-
+            <Route path="/create" component={ CreateProject } />
           </Switch>
         </div>
       </BrowserRouter>
