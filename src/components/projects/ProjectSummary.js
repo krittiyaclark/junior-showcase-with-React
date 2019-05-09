b879896f551972a641f0d453b14c7e91ecf6984f
krittiyaclark <krittiyaclark@gmail.com>
2019-05-09 16:21:50 -0400

    Adding Projects & display author name

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
         <div className="card project-summary">
             <div className="card-content text-secondary">
                 <h5 className="card-title">{project.title}</h5>
-                <p className="card-text">Posted by the Wippo</p>
+                <p className="card-text">Posted by {project.authorFirstName} {project.authorLastName}</p>
                 <p className="text-secondary">1st July, 9am</p>
             </div>
         </div>
