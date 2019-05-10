6f957b8835797b1ef6ca5ad6383fd517b5ed1bf8
krittiyaclark <krittiyaclark@gmail.com>
2019-05-10 15:10:34 -0400

    A Simple Cloud Function & Notification Component

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Notifications = () => {
     return (
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card">
+                <div className="card-content">
+                    <div className="card-title">
+                        <h3>Notifications</h3>
+                        <ul className="notifications">
+                            <li>Notifications</li>
+                            <li>Notifications</li>
+                            <li>Notifications</li>
+                            <li>Notifications</li>
+                            <li>Notifications</li>                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
