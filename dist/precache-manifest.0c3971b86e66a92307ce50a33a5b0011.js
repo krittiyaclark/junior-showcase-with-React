55679119af2674897d19ad2bf2b097423c043c16
krittiyaclark <krittiyaclark@gmail.com>
2019-06-24 17:20:29 -0400

    Run npm build

---
+++ dist/precache-manifest.0c3971b86e66a92307ce50a33a5b0011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0350d7da9427bc2e8635",
+    "url": "/static/css/main.b329271c.chunk.css"
+  },
+  {
+    "revision": "0350d7da9427bc2e8635",
+    "url": "/static/js/main.bc8333f1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9f7df8dfa197cf5032e1",
+    "url": "/static/js/2.9615b3db.chunk.js"
+  },
+  {
+    "revision": "178d980c31d331d720f3c3d01993d155",
+    "url": "/index.html"
+  }
+];
